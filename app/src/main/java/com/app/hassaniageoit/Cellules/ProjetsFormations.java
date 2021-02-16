981a08a43e47e2e45c69b0fa6224c85b29623f44
Farkadi06 <58217000+Farkadi06@users.noreply.github.com>
2021-02-16 18:17:25 +0100

    Update the HomeActivity UI and Creating the ProjetFormation UI

---
+++ app/src/main/java/com/app/hassaniageoit/Cellules/ProjetsFormations.java
@@ -0,0 +1,16 @@
+package com.app.hassaniageoit.Cellules;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.hassaniageoit.R;
+
+public class ProjetsFormations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_projets_formations);
+    }
+}
--- app/src/main/java/com/app/hassaniageoit/HomeActivity.java
+++ app/src/main/java/com/app/hassaniageoit/HomeActivity.java
@@ -3,10 +3,16 @@ package com.app.hassaniageoit;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import com.app.hassaniageoit.Cellules.ProjetsFormations;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -16,8 +22,23 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+       // setSupportActionBar(toolbar);
+
+        final CardView ProjetFormationCard = findViewById(R.id.ProjetFormationID);
+        CardView EventsCard = findViewById(R.id.EventsID);
+        CardView DesignID = findViewById(R.id.DesignID);
+        CardView MediaID = findViewById(R.id.MediaID);
+        CardView SponsoringID = findViewById(R.id.SponsoringID);
+
+        ProjetFormationCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent HomeIntent = new Intent(HomeActivity.this, ProjetsFormations.class);
+                startActivity(HomeIntent);
+            }
+        });
+
 }
 
 
